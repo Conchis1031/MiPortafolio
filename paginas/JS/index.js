ed90860a8a442376784dc0edd683ea61b0b30bcf
CarolinaAyala <carolina.ayala1031@gmail.com>
2023-11-28 20:55:56 -0600

    se coloca navbar

--- paginas/JS/index.js
+++ paginas/JS/index.js
@@ -1,66 +1,17 @@
-/*----------------------------------------------------*/
-/* Quote Loop
------------------------------------------------------- */
-
-function fade($ele) {
-    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
-        var $next = $(this).next('.quote');
-        fade($next.length > 0 ? $next : $(this).parent().children().first());
-   });
-}
-fade($('.quoteLoop > .quote').first());
-
-
-/*----------------------------------------------------*/
-/* Navigation
------------------------------------------------------- */
-
-$(window).scroll(function() {
-
-    if ($(window).scrollTop() > 300) {
-        $('.main_nav').addClass('sticky');
-    } else {
-        $('.main_nav').removeClass('sticky');
+document.addEventListener('DOMContentLoaded', function () {
+    // Función para desplazarse a una sección específica
+    function scrollToSection(sectionId) {
+      document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
     }
-});
-
-// Mobile Navigation
-$('.mobile-toggle').click(function() {
-    if ($('.main_nav').hasClass('open-nav')) {
-        $('.main_nav').removeClass('open-nav');
-    } else {
-        $('.main_nav').addClass('open-nav');
-    }
-});
-
-$('.main_nav li a').click(function() {
-    if ($('.main_nav').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.main_nav').removeClass('open-nav');
-    }
-});
-
-
-/*----------------------------------------------------*/
-/* Smooth Scrolling
------------------------------------------------------- */
-
-jQuery(document).ready(function($) {
-
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash,
-	    $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
   
-});
-
-
-TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
+    // Agregar eventos de clic a los enlaces de navegación
+    document.querySelectorAll('.smoothscroll').forEach(function (link) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        // Extraer el destino del atributo href
+        var targetId = this.getAttribute('href').substring(1);
+        scrollToSection(targetId);
+      });
+    });
+  });
+  
